f298e9a86b3f8e15a87c7c01692aeaf03e5ed726
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-04-17 08:47:22 -0700

    finished Qs and file structure

---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    isAdding: false,
+    error: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default reducer
